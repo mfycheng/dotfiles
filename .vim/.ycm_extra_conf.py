a054a887115f9045091a66520aa8c821759d392c
Mike Cheng <mfycheng@uwaterloo.ca>
2015-07-01 02:47:41 -0400

    Remove c98 compat warning

--- .vim/.ycm_extra_conf.py
+++ .vim/.ycm_extra_conf.py
@@ -38,7 +38,6 @@ flags = [
 '-Wall',
 '-Wextra',
 '-Werror',
-'-Wc++98-compat',
 '-Wno-long-long',
 '-Wno-variadic-macros',
 '-fexceptions',
@@ -59,7 +58,7 @@ flags = [
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x', 'c++',
 '-isystem', '../BoostParts',
-# This path will only work on OS X, but extra paths that don't exist are not 
+# This path will only work on OS X, but extra paths that don't exist are not
 # harmful
 '-isystem',  '/System/Library/Frameworks/Python.framework/Headers',
 '-isystem', '../llvm/include',
